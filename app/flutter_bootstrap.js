a56aef41aaa4c0721a2dca0af2c151f113dd49c5
rig-community <mukmen26@gmail.com>
2026-07-01 17:05:39 +0700

    Fix dart io web

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3604030079" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3802110431" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
