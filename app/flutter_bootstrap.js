2393327568c0a3785a29f6f6ced57645546a610f
rig-community <mukmen26@gmail.com>
2026-07-01 16:25:59 +0700

    Fix web platform errors

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1815889735" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2306925767" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
