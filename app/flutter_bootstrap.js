361df16a7d5101479461fd72aaf7ab9c07f0406a
rig-community <mukmen26@gmail.com>
2026-07-01 10:13:30 +0700

    Rebuild with correct base href

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "802859421" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1815889735" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
